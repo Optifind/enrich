8adfe4888c4faedfb93b29b0f3389104be92fce0
lattots <otso@lattu.biz>
2024-08-02 01:09:51 +0300

    Fix load dump script

--- scripts/load_db_dump/load.sh
+++ scripts/load_db_dump/load.sh
@@ -9,8 +9,14 @@ dbname=$(echo $DATABASE_URL | sed -n '\/([^\/]+)$')
 # Password is exported to environment variable so pg_restore can use it
 export PGPASSWORD=$password
 
+echo "$username"
+echo "$password"
+echo "$host"
+echo "$port"
+echo "$dbname"
+
 # Database is restored with dump file
-pg_restore --verbose --clean --no-acl --no-owner -h $host -p $port -U $username -d $dbname data/db_dumps/latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h $host -p $port -U $username -d $dbname -f data/db_dumps/latest.dump
 
 # PGPASSWORD is removed from environment variables
 unset PGPASSWORD
