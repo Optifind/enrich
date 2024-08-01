102bee68d0359274f20c509ee1e51438bb81f65c
lattots <otso@lattu.biz>
2024-08-01 21:46:42 +0300

    Get database URL from env variable

--- scripts/load_db_dump/load.sh
+++ scripts/load_db_dump/load.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Run pg_restore with the parsed components
-psql -f data/db_dumps/latest.sql
+# Check if DATABASE_URL is set
+if [ -z "$DATABASE_URL" ]; then
+  echo "Error: DATABASE_URL is not set."
+  exit 1
+fi
+
+# Load database from dump file
+psql $DATABASE_URL -f data/db_dumps/latest.sql
